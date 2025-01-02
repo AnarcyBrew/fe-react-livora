dd3478bc6878452e326fd6d65f84c5b0f3e64636
Andrianto Putro <andri.c.putro@gmail.com>
2025-01-02 16:49:22 +0700

    Translate html to react : BrowseRoom

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import Header from "../parts/Header";
 import Hero from "../parts/Hero";
 import JustArrived from "../parts/HomePage/JustArrived";
+import BrowseRoom from "../parts/HomePage/BrowseRoom";
 
 export default function HomePage() {
     return (
         <>
             <Header/>
             <Hero/>
+            <BrowseRoom/>
             <JustArrived/>
         </>
     )
