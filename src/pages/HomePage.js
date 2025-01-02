c6e2f0f46b8aafaca2b104758fb15fc3af4b4a38
Andrianto Putro <andri.c.putro@gmail.com>
2025-01-02 16:42:29 +0700

    Translate HTML to ReactJS : JustArrived

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import Header from "../parts/Header";
 import Hero from "../parts/Hero";
+import JustArrived from "../parts/HomePage/JustArrived";
 
 export default function HomePage() {
     return (
         <>
             <Header/>
             <Hero/>
+            <JustArrived/>
         </>
     )
 }
